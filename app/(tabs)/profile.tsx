fc2f949e3f53b1c5c9d739cc4b9685241df1b3f9
ricky <richardwoesiii@gmail.com>
2023-10-02 18:05:45 -0400

    removed filler from profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -8,7 +8,6 @@ export default function Profile() {
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
