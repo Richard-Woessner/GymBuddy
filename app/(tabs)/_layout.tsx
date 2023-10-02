0bf9537ed5a1a0272f0cd4cd8987e8af57c1bb79
ricky <richardwoesiii@gmail.com>
2023-10-02 16:30:11 -0400

    GB-navBar: added icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,15 +4,13 @@ import { Pressable, useColorScheme } from 'react-native';
 
 import Colors from '../../constants/Colors';
 import ProfileScreen from './profile';
+import { Ionicons } from '@expo/vector-icons';
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
+  return <Ionicons size={32} {...props} />;
 }
 
 export default function TabLayout() {
@@ -28,21 +26,21 @@ export default function TabLayout() {
         name="profile"
         options={{
           title: 'profile title',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="person-outline" color={color} />,
         }}
       />
       <Tabs.Screen
         name="logs"
         options={{
           title: 'logs title',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="book-outline" color={color} />,
         }}
       />
       <Tabs.Screen //THIS IS JUST FOR FLAVOR, ANY TSX FILE IN TABS WILL CREATE A TAB
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="home-outline" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -63,14 +61,14 @@ export default function TabLayout() {
         name="trainer"
         options={{
           title: 'trainer',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="person-add-outline" color={color} />,
         }}
       />
       <Tabs.Screen
         name="messages"
         options={{
           title: 'messages',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="chatbubbles-outline" color={color} />,
         }}
       />
     </Tabs>
