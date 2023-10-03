27210c6e9e602cfe34423cc2af91888d5cc1dc53
ricky <richardwoesiii@gmail.com>
2023-10-03 19:48:34 -0400

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ export default function TabOneScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.title}>Welcome to GymBuddy</Text>
+      <Text style={styles.title}>Welcome to GymBuddy test</Text>
     </View>
   );
 }
