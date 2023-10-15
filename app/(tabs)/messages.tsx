e0b167a1641e1aa87bea689b74bd0894bcb36408
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-10-15 16:24:53 -0400

    Created a simple messaging section

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -1,12 +1,43 @@
-import { StyleSheet } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { Text, View } from '../../components/Themed';
 
 export default function Profile() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [text, setText] = useState<string>('');
+
+
+  const handleSend = () => {
+    if (text.trim() !== '') {
+      setMessages([...messages, text]);
+      setText('');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Messages</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+      {/* Display Messages */}
+      <ScrollView style={styles.messageContainer}>
+        {messages.map((message, index) => (
+          <Text key={index} style={styles.messageText}>{message}</Text>
+        ))}
+      </ScrollView>
+
+      {/* Input for typing messages */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type a message"
+          value={text}
+          onChangeText={(value) => setText(value)}
+        />
+        <TouchableOpacity onPress={handleSend} style={styles.sendButton}>
+          <Text style={styles.sendButtonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -20,10 +51,47 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 10,
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
     width: '80%',
+  },
+  messageContainer: {
+    flex: 1,
+    width: '100%',
+    padding: 10,
+  },
+  messageText: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+  input: {
+    flex: 1,
+    marginRight: 10,
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 20,
+  },
+  sendButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  sendButtonText: {
+    color: 'white',
   },
 });
