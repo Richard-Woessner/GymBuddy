3abe119c6eb76a412d37bce3bb9b83311664a91e
Richard Woessner <71599819+Richard-Woessner@users.noreply.github.com>
2023-10-19 11:25:33 -0400

    added nvmrc, added axios (#39)
    * added nvmrc, added axios
    
    * added workoutservice
    
    * Reading workout from backend
    
    * removed logs

---
+++ services/workoutService.ts
@@ -0,0 +1,41 @@
+import { AxiosService } from './axiosService';
+
+class WorkoutService extends AxiosService {
+  public constructor() {
+    super();
+  }
+
+  async getWorkouts() {
+    try {
+      const { data } = await this.axios.get<GetWorkoutResponse>('/api/workouts');
+      return data;
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+}
+
+export interface GetWorkoutResponse {
+  Workouts: Workout[];
+  User: string;
+}
+
+export interface Workout {
+  Name: string;
+  Exersizes: Exersize[];
+}
+
+interface Exersize {
+  Exersize: string;
+  Sets: Set[];
+  Type: string;
+}
+
+interface Set {
+  SetNumber: number;
+  Reps: number;
+  Weight: number;
+}
+
+export const workoutService = new WorkoutService();
