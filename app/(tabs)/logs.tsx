723dd5328c318b6fa04b2be3ffa2159c747ea75e
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-10-24 20:37:44 -0400

    Created log page where workout cards are displayed

--- app/(tabs)/logs.tsx
+++ app/(tabs)/logs.tsx
@@ -1,28 +1,41 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import { View, Text,  } from '../../components/Themed';
 
-import { Text, View } from '../../components/Themed';
+//Hard coded workouts. Will be deleted 
+const workouts = [
+  { id: 1, exercise: 'Push-ups', sets: 3, reps: 15 },
+  { id: 2, exercise: 'Squats', sets: 3, reps: 12 },
+  { id: 3, exercise: 'Jog', sets: 1, duration: '15 minutes' },
+];
 
-export default function Logs() {
+const Logs = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Logs</Text>
-    </View>
+    <ScrollView>
+      {workouts.map((workout) => (
+        <View key={workout.id} style={styles.card}>
+          <Text style={styles.cardTitle}>{workout.exercise}</Text>
+          <Text>{`Sets: ${workout.sets || '-'}`}</Text>
+          <Text>{`Reps: ${workout.reps || '-'}`}</Text>
+          <Text>{`Duration: ${workout.duration || '-'}`}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  card: {
+    backgroundColor: 'white',
+    padding: 16,
+    margin: 16,
+    borderRadius: 8,
+    elevation: 3,
   },
-  title: {
-    fontSize: 20,
+  cardTitle: {
+    fontSize: 18,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
 });
+
+export default Logs;
